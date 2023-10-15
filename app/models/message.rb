c5717db6914dc7b78c3ca2e68d0ce014f30925e5
Yuriy Kotlyarov <yury.kotlyarov@gmail.com>
2023-10-15 23:19:27 +0300

    feat: Flash для всех открытых сессий

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,8 @@
 class Message < ApplicationRecord
   belongs_to :chat
 
-  after_create_commit { broadcast_append_to('messages') }
+  after_create_commit do
+    broadcast_append_to 'messages'
+    broadcast_append_to 'flash', target: 'flash', partial: 'shared/flash', locals: { message: self }
+  end
 end
