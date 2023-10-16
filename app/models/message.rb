c767d6236f54217f49ad72eb745f593068517c61
Yuriy Kotlyarov <yury.kotlyarov@gmail.com>
2023-10-16 09:13:48 +0300

    fix: Correct channels and targets

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,9 @@
 class Message < ApplicationRecord
+  include ActionView::RecordIdentifier
   belongs_to :chat
 
   after_create_commit do
-    broadcast_append_to 'messages'
+    broadcast_append_to dom_id(chat, 'messages'), target: dom_id(chat, 'messages')
     broadcast_append_to 'flash', target: 'flash', partial: 'shared/flash', locals: { message: self }
   end
 end
